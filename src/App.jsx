7295040bf394cf9fcf7891cb41cacd302c4aeca1
Umidjon-developer <umidjongafforov175@gmail.com>
2024-10-13 23:40:23 +0500

    src

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,6 @@ const App = () => {
 
   return (
     <>
-      <h1 className="heading">Sammi kurslar</h1>
       <Cart cartItems={cartItems} onCheckout={onCheckout} />
       <div className="cards__container">
         {courses.map((course) => (
